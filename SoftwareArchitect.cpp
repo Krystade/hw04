c6f9ce66c1164e3e893a9df717bad3ae29df1184
Krys <21113012+Krystade@users.noreply.github.com>
2020-02-13 18:48:59 -0800

    Update SoftwareArchitect.cpp

--- SoftwareArchitect.cpp
+++ SoftwareArchitect.cpp
@@ -8,12 +8,14 @@
 
 #include "SoftwareArchitect.h"
 
+//Default SoftwareArchitect constructor
 SoftwareArchitect::SoftwareArchitect():Employee(){
 	departmentNum = 0;
 	supervisorName = " ";
 	percentSalaryIncrease = 0.00;
 	yearsExp = 0;
 }
+//Overloaded SoftwareArchitect constructor
 SoftwareArchitect::SoftwareArchitect(string name, int id, string phone, int age, char gender, string title, int salary, Date* hireDate, int departmentNum, string supervisorName, double percentSalaryIncrease, int yearsExp):Employee(name, id, phone, age, gender, title, salary, hireDate){
 	this->departmentNum = departmentNum;
 	this->supervisorName = supervisorName;
@@ -21,6 +23,7 @@ SoftwareArchitect::SoftwareArchitect(string name, int id, string phone, int age,
 	this->yearsExp = yearsExp;
 }
 
+//Function that prints out a SoftwareArchitect object's data using cout
 void SoftwareArchitect::print(){
 	Employee(*this).print();
 	cout << "Department Number: " << departmentNum << "\nSupervisor's Name: " << supervisorName
