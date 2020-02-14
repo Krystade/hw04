df7b8952396013fbc680f05fc5f3196d9701358d
Krys <21113012+Krystade@users.noreply.github.com>
2020-02-13 18:07:25 -0800

    Update Date.cpp

--- Date.cpp
+++ Date.cpp
@@ -7,17 +7,20 @@
 
 #include "Date.h"
 
+//Default Date constructor
 Date::Date(){
 	month = 0;
 	day = 0;
 	year = 0;
 }
+//Overloaded Date constructor
 Date::Date(int m, int d, int y){
 	month = m;
 	day = d;
 	year = y;
 }
 
+//Fuction that prints out a Date object's data using cout
 void Date::print(){
 	cout << month << "/" << day << "/" << year << endl;
 }
